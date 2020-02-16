a19194a51dcd4bc6db2c505614ce60ae527578af
Jake Vossen <jake@vossen.dev>
2020-02-15 19:08:12 -0700

    working on from string for nfa row

---
+++ nfamatch/src/row.rs
@@ -0,0 +1,46 @@
+use std::fmt;
+use std::str::FromStr;
+
+#[derive(Debug, Default)]
+pub struct Row {
+    accepting_state: bool,
+    pub id: usize,
+    transitions: Vec<char>,
+}
+
+impl Row {
+    fn new(accepting_state: bool, id: usize, transitions: Vec<char>) -> Self {
+        Self {
+            accepting_state,
+            id,
+            transitions,
+        }
+    }
+}
+
+impl fmt::Display for Row {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // writeln!(f, "{}", something)...
+        todo!()
+    }
+}
+
+impl FromStr for Row {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let tokens: Vec<&str> = input.split(" ").collect();
+
+        match tokens.as_slice() {
+            [accept, id, transitions @ ..] => {
+                let is_accept = *accept == "+";
+                let id = id.parse().unwrap();
+                let transitions: Vec<char> =
+                    transitions.iter().map(|s| s.parse().unwrap()).collect();
+
+                Ok(Row::new(is_accept, id, transitions))
+            }
+            _ => unreachable!(),
+        }
+    }
+}
